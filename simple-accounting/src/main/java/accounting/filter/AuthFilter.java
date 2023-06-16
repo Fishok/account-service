16cdce11314371a323ddb41d646971bd3ed538c1
Alex <sashafish93@gmial.com>
2023-06-16 09:52:27 +0300

    Added secure deps

--- simple-accounting/src/main/java/accounting/filter/AuthFilter.java
+++ simple-accounting/src/main/java/accounting/filter/AuthFilter.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import jakarta.servlet.FilterChain;
@@ -11,19 +14,34 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class AuthFilter extends OncePerRequestFilter{
+public class AuthFilter extends OncePerRequestFilter {
 
-	
-	private static final List<String> PUBLIC_ENDPOINTS = Arrays.asList(
-			"/api/accounts/create",
-			"/api/accounts/login"
-			);
-	
+	private static final List<String> PUBLIC_ENDPOINTS = Arrays.asList("/api/accounts/create", "/api/accounts/login");
+
+	private static final AntPathMatcher PATH_MATCHER = new AntPathMatcher();
+
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		
+		String requestUri = request.getRequestURI();
+		if (isPublicEndpoint(requestUri)) {
+			filterChain.doFilter(request, response);
+			return;
+		}
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (authentication == null || !authentication.isAuthenticated()) {
+			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+			return;
+		}
+		filterChain.doFilter(request, response);
+	}
+
+	private boolean isPublicEndpoint(String requestURI) {
+		for (String publicEndpoint : PUBLIC_ENDPOINTS) {
+			if (PATH_MATCHER.match(publicEndpoint, requestURI))
+				return true;
+		}
+		return false;
 	}
 
 }
